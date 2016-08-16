a568d089e6581209ee3ec5bed72c3d1e9d6c9fe1
Bishnu <bishnucit@gmail.com>
2016-08-16 21:46:06 +0530

    Node js and mysql
    Enhanced the sign up feature where user data is stored in db (mysql) and
    user gets redirected to a new page of /message displaying user
    successful registration.
    
    This is continuation of work from the site
    http://codehandbook.org/web-app-using-node-js-and-mysql/
    
    Licence : MIT

---
+++ app.js
@@ -0,0 +1,69 @@
+var express = require('express');
+var app = express();
+var mysql = require('mysql');
+var connection = mysql.createConnection({
+
+  host     : 'localhost',
+  user     : 'root',
+  password : 'root',
+  database : 'mydb'
+});
+var bodyParser = require('body-parser');
+app.use(bodyParser.json()); // for parsing application/json
+app.use(bodyParser.urlencoded({ extended: true })); // for parsing application/x-www-form-urlencoded
+
+	
+//connection.connect();
+
+connection.query('SELECT * from mytable1', function(err, rows, fields) {
+  if (!err)
+    console.log('The solution is: ', rows);
+  else
+    console.log('Error while performing Query.');
+});
+
+//connection.end();
+// Binding express app to port 3000
+app.listen(3000,function(){
+    console.log('Node server running @ http://localhost:3000')
+});
+
+
+app.use('/node_modules',  express.static(__dirname + '/node_modules'));
+
+app.use('/style',  express.static(__dirname + '/style'));
+
+app.get('/',function(req,res){
+    res.sendFile('home.html',{'root': __dirname + '/templates'});
+});
+
+
+app.get('/showSignInPage',function(req,res){
+    res.sendFile('signin.html',{'root': __dirname + '/templates'});
+});
+
+app.get('/showSignUpPage',function(req,res){
+  res.sendFile('signup.html',{'root':__dirname + '/templates'})
+});
+
+app.get('/message',function(req,res){
+    res.sendFile('message.html',{'root': __dirname + '/templates'});
+});
+
+app.post('/myaction', function(req, res) {
+	console.log('req.body');
+	console.log(req.body);
+	var record = {email: req.body.email, pass: req.body.pass};
+
+	//connection.connect();
+	connection.query('INSERT INTO mytable1 SET ?', record, function(err,res){
+	  	if(err) throw err;
+		console.log('Last record insert id:', res.insertId);
+		
+	});
+
+	res.redirect('/message');
+	//connection.end();
+
+	res.end();
+});
